63d37b898ffd59fbca323d5fc431681ff47091d2
taoky <me@taoky.moe>
2025-04-06 22:06:55 +0800

    docs: Add mygo-gen

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # mortis
 
-## Methods
+> Mortis 就像是看了 10000 次 MyGO 的群友，后面忘了。
 
-- m1：把整个台词集全塞到 prompt 里面，让模型选择（4k 行台词大约 40k token）
+## 生成台词集合
+
+参考 mygo-gen.py（需要安装 pysub2），对应的字幕组仓库是 <https://github.com/Nekomoekissaten-SUB/Nekomoekissaten-Storage>。需要注意的是，该字幕的 license 是 CC BY-NC-ND 4.0，这意味着脚本生成的文件（以及对应的 embedding）无法共享，需要自己动手操作。
+
+也可以自己收集去重，每行一句即可。
+
+## 回复生成方法
+
+三种参考方法：
+
+- m1：把整个台词集全塞到 prompt 里面，让模型选择（4k 行台词大约 40k token，按每 1M token 2 元来算的话，100 块可以推理个大约 1250 次，有点小贵）
 - m2：分两步，第一步让模型提供适合回复的关键词，搜索后第二步让模型选择
-- m3：分两步，第一步生成每一句台词的 embedding，第二步让模型输出回复，回复也生成 embedding，选择最相似的回复
+- m3：分两阶段，第一阶段生成每一句台词的 embedding 保存到本地，第二阶段让模型输出回复，回复也生成 embedding，选择最相似的回复（容易前言不搭后语）
---
+++ playground/mygo-gen.py
@@ -0,0 +1,68 @@
+import pysubs2
+import glob
+import os
+import re
+from collections import defaultdict
+
+words = defaultdict(set)
+FILENAME_REGEX = re.compile(r"MyGO!!!!! \[(\d\d)\]")
+
+SKIPS = [
+    "m 0",
+    "~~",
+    "→",
+    "本字幕由",
+    re.compile(r"「[　　　  　　　]+」"),
+    "注：",
+]
+REPLACEMENTS = {
+    "公司让我不能跟家人之外\\N的人说，但我无论如何都\\N想告诉你": "公司让我不能跟家人之外的人说，但我无论如何都想告诉你",
+    "　纷纷雪　": "「纷纷雪」",
+    "　印影散散　": "「印影散散」",
+    "　短篇集 寿喜烧": "「短篇集 寿喜烧」",
+    "当我注意到自己心中\\N涌出的感情时，已经完全被带着走\\N了，不知为何咬紧了牙握紧了拳头，\\N甚至哭了起来。": "当我注意到自己心中涌出的感情时，已经完全被带着走了，不知为何咬紧了牙握紧了拳头，甚至哭了起来。",
+    "虽然我还有些没整理好情绪，但这个\\N乐队真的很神奇。等我冷静了再写感\\N想。": "虽然我还有些没整理好情绪，但这个乐队真的很神奇。等我冷静了再写感想。",
+    "我以为会一团糟，到结束却觉得\\N感动？兴奋？": "我以为会一团糟，到结束却觉得感动？兴奋？",
+    "今天的Live真精彩。乐队叫\\NCRYCHIC。吉他、键盘还有鼓都\\N不错，贝斯也挺熟练，挺有好感的。": "今天的Live真精彩。乐队叫CRYCHIC。吉他、键盘还有鼓都不错，贝斯也挺熟练，挺有好感的。"
+}
+
+for i in glob.glob("Nekomoekissaten-Storage/BanG_Dream/MyGO/*.JPSC.ass"):
+    filename = os.path.basename(i)
+    matches = FILENAME_REGEX.search(filename)
+    assert matches
+    ep = matches.group(1)
+    # subs = pysubs2.load(i)
+    with open(i) as f:
+        file_contents = f.read()
+        for r in REPLACEMENTS:
+            file_contents = file_contents.replace(r, REPLACEMENTS[r])
+        subs = pysubs2.SSAFile.from_string(file_contents)
+    for line in subs:
+        if "JP" in line.style or line.style == "Default":
+            continue
+        text = line.plaintext
+        is_skip = False
+        for s in SKIPS:
+            if type(s) == str and s in text or type(s) == re.Pattern and s.search(text):
+                is_skip = True
+                break
+        if is_skip:
+            continue
+        parts = text.split("\n")
+        for p in parts:
+            p = p.strip()
+            if not p:
+                continue
+            if p.endswith("，") or p.endswith(","):
+                p = p.rstrip("，,")
+            if p.startswith("「") and "」" not in p:
+                p = p.lstrip("「")
+            words[p].add(ep)
+
+# print(words)
+
+with open("mygo.txt", "w") as f:
+    for word in sorted(words):
+        # for id in sorted(words[word]):
+        #     print(id, end=";", file=f)
+        print("", word, file=f)
